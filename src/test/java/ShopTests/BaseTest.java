0a524d9737114e710be8f416b03f50c6ec74b357
Wojciech Wroblewski <wuwu@op.pl>
2022-12-20 10:45:09 +0100

    changes for grid

--- src/test/java/ShopTests/BaseTest.java
+++ src/test/java/ShopTests/BaseTest.java
@@ -1,10 +1,11 @@
 package ShopTests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.logging.Log;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ITestResult;
@@ -13,9 +14,10 @@ import utils.AllureUtils;
 import utils.DriverProvider;
 import utils.ScreenshotUtil;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
 
 public class BaseTest {
     public WebDriver driver;
@@ -23,14 +25,23 @@ public class BaseTest {
 //    deklaracja logera w klasie bazowej zapewnia dostep do niego w klasach pochodnych
 
     @BeforeClass(alwaysRun = true)
-    public void setUp(){
+    public void setUp() throws MalformedURLException {
         LOGGER.info("startuje klase " + this.getClass());
-        driver = DriverProvider.getDriver(System.getProperty("TestDriver"));
-//        System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
-//        WebDriverManager.firefoxdriver().setup();
-//        driver = new FirefoxDriver();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-//        driver.manage().window().maximize();
+
+        FirefoxOptions ffOptions = new FirefoxOptions();
+        ffOptions.setCapability("platformName", "Windows");
+//        ffOptions.setCapability("browserVersion", "108.0.1");
+//        W przypadku wykorzystania Grid 4 zalecane jest wykorzyanie obiekty klas Options (FirefoxOption/ChromeOptions)
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setBrowserName("firefox");
+//        cap.setVersion("108");
+//        W starszych wersjach zazwycaj wykorzystywało się obiekty klasy DesiredCapabilities
+
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"), ffOptions);
+//        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+//        Dla Selenium Grid 4 nie musimy dodawać do adresu endpointa /wd/hub (jak miało to miejsce w starszych wersjach)
+        driver.get("http://sampleshop.inqa.pl/");
     }
 
     @BeforeMethod
@@ -55,7 +66,7 @@ public class BaseTest {
     public void tearDown(){
         AllureUtils.reportEnv("firefox", "11", "prod");
         LOGGER.info("Kończę testy w klasie " + this.getClass());
-//        driver.quit();
-        DriverProvider.quitDriver();
+        driver.quit();
+//        DriverProvider.quitDriver();
     }
 }
